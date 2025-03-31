9e7950b845c811ae0cea006dd8c2287f086e196f
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2025-03-31 02:32:36 +0200

    edit cartItems onclick + checkboxes for boolean options

--- app/components/cart-item/CartItem.tsx
+++ app/components/cart-item/CartItem.tsx
@@ -1,10 +1,13 @@
-import { Edit, Minus, Plus, Trash2 } from "lucide-react";
+import { Edit, Minus, Plus, Trash2 } from 'lucide-react';
+import ConfirmationDialog from '~/components/Menu/ConfirmationDialog';
+import { Button } from '~/components/ui/button';
+import { Checkbox } from '~/components/ui/checkbox';
+import { Dialog, DialogTrigger } from '~/components/ui/dialog';
 import {
   selectItem,
   setDecrementItem,
   setIncrementItem,
-} from "~/stores/CartStore";
-import { Button } from "../ui/button";
+} from '~/stores/CartStore';
 
 interface IProps {
   cartId: string;
@@ -17,75 +20,97 @@ export default function CartItem({ cartId }: IProps) {
   const handleDecrease = () => setDecrementItem(cartId);
 
   const totalPrice = (
-    parseFloat((price ?? "0").replace(",", ".")) * (quantity ?? 0)
+    parseFloat((price ?? '0').replace(',', '.')) * (quantity ?? 0)
   ).toFixed(2);
 
   return (
-    <div className="flex flex-col rounded-lg border bg-white shadow-sm transition-shadow hover:shadow-md">
-      {/* Main Content */}
-      <div className="p-4">
-        {/* Name, Basic Price, and Edit Icon */}
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-2">
-            <h3 className="text-lg font-semibold">{name}</h3>
-            <p className="text-gray-700">({price}€)</p>
+    <Dialog>
+      <DialogTrigger asChild>
+        <div className="flex flex-col rounded-lg border bg-white shadow-sm transition-shadow hover:shadow-md">
+          {/* Main Content */}
+          <div className="p-4">
+            {/* Name, Basic Price, and Edit Icon */}
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <h3 className="text-lg font-semibold">{name}</h3>
+                <p className="text-gray-700">({price}€)</p>
+              </div>
+              <Edit className="m-2 h-4 w-4" />
+            </div>
+
+            {/* Options */}
+            {options && Object.keys(options).length > 0 && (
+              <div className="mt-2">
+                <p className="text-sm text-gray-600">
+                  Options: ( {Object.keys(options).length} )
+                </p>
+                <ul className="list-inside list-disc">
+                  {Object.entries(options).map(([key, value], index) => (
+                    <li key={index} className="text-sm text-gray-600">
+                      {key}:{' '}
+                      {value === 'true' || value === 'false' ? (
+                        <Checkbox checked={value === 'true'} />
+                      ) : (
+                        value
+                      )}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+
+            {/* Note */}
+            {note && (
+              <div className="mt-2 rounded-lg border p-2">
+                <p className="text-sm text-gray-600">{note}</p>
+              </div>
+            )}
           </div>
-          <Edit className="m-2 h-4 w-4" />
+
+          {/* Quantity Controls and Total Price Banner */}
+          <div className="rounded-b-lg border-t bg-gray-50 p-4">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <Button
+                  variant="outline"
+                  size="sm"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleDecrease();
+                  }}
+                  className="flex h-8 w-8 items-center justify-center p-0"
+                >
+                  {quantity === 1 ? (
+                    <Trash2 className="h-4 w-4" />
+                  ) : (
+                    <Minus className="h-4 w-4" />
+                  )}
+                </Button>
+                <span className="text-lg font-medium">{quantity}</span>
+                <Button
+                  variant="outline"
+                  size="sm"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    handleIncrease();
+                  }}
+                  className="flex h-8 w-8 items-center justify-center p-0"
+                >
+                  <Plus className="h-4 w-4" />
+                </Button>
+              </div>
+              <p className="text-lg font-semibold">{totalPrice}€</p>
+            </div>
+          </div>
         </div>
-
-        {/* Options */}
-        {options && Object.keys(options).length > 0 && (
-          <div className="mt-2">
-            <p className="text-sm text-gray-600">
-              Options: ( {Object.keys(options).length} )
-            </p>
-            <ul className="list-inside list-disc">
-              {Object.entries(options).map(([key, value], index) => (
-                <li key={index} className="text-sm text-gray-600">
-                  {key}: {value}
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
-
-        {/* Note */}
-        {note && (
-          <div className="mt-2 rounded-lg border p-2">
-            <p className="text-sm text-gray-600">{note}</p>
-          </div>
-        )}
-      </div>
-
-      {/* Quantity Controls and Total Price Banner */}
-      <div className="rounded-b-lg border-t bg-gray-50 p-4">
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-2">
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={handleDecrease}
-              className="flex h-8 w-8 items-center justify-center p-0"
-            >
-              {quantity === 1 ? (
-                <Trash2 className="h-4 w-4" />
-              ) : (
-                <Minus className="h-4 w-4" />
-              )}
-            </Button>
-            <span className="text-lg font-medium">{quantity}</span>
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={handleIncrease}
-              className="flex h-8 w-8 items-center justify-center p-0"
-            >
-              <Plus className="h-4 w-4" />
-            </Button>
-          </div>
-          <p className="text-lg font-semibold">{totalPrice}€</p>
-        </div>
-      </div>
-    </div>
+      </DialogTrigger>
+      <ConfirmationDialog
+        cartId={cartId}
+        id={id!}
+        title={name!}
+        selectedPrice={price!}
+        options={Object.keys(options!)}
+      />
+    </Dialog>
   );
 }
