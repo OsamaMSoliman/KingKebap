5e0b48d39de3fed4f12d4c65bdcd0d7cc2ca8941
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2025-04-04 12:06:10 +0200

    fixed footer animation and added the alert for the closing times

--- app/components/about-us/AboutUs.tsx
+++ app/components/about-us/AboutUs.tsx
@@ -1,58 +1,75 @@
-import { MapPinIcon, PhoneIcon, TruckIcon } from "lucide-react";
+import { AlertTriangle, MapPinIcon, PhoneIcon, TruckIcon } from 'lucide-react';
+// This is not required, but I wanted to test out both kinds of css imports
+import './AboutUs.css';
+import styles from './AboutUs.module.css';
 
 interface IProps {
-  isOpen?: boolean;
+  isExpanded?: boolean;
+  isCurrentlyClosed: boolean;
 }
 
-export default function AboutUs({ isOpen = false }: IProps) {
-  // TODO: Hinweis: Wir haben gerade geschlossen. Wenn Sie möchten, können Sie aber jetzt schon bestellen, wir liefern dann zu unseren Geschäftszeiten.
-  return isOpen ? (
+export default function AboutUs({
+  isCurrentlyClosed,
+  isExpanded = false,
+}: IProps) {
+  return isExpanded ? (
     <div className="space-y-2">
       <p>Über uns</p>
-      <table className="mx-auto table-auto border-separate border-spacing-x-5 border-spacing-y-1">
-        <caption className="caption-top">
-          <TruckIcon className="mx-1 inline-block h-5 w-5" />
-          Lieferung ab 15€ Bestellwert frei Haus
-        </caption>
-        <tbody>
-          <tr>
-            <td>Taglich:</td>
-            <td>12:00-22:00 Uhr</td>
-          </tr>
-          <tr>
-            <td>Sonn- & Feiertage:</td>
-            <td>13:00-22:00 Uhr</td>
-          </tr>
-        </tbody>
-        <caption className="caption-bottom">
-          <MapPinIcon className="mx-1 inline-block h-5 w-5" />
-          Bachstrase 2 • 58762 Altena
-        </caption>
-      </table>
-      <p>
-        <PhoneIcon className="mx-1 inline-block h-5 w-5" />
-        02352 - 2670780
-      </p>
+      <InfoAboutUs />
+      {isCurrentlyClosed && (
+        <div className="relative overflow-hidden">
+          <div className="animate-scrollHorizontally inline-flex space-x-8">
+            <CurrentlyClosedAlert />
+            <CurrentlyClosedAlert /> {/* Duplicate for seamless looping */}
+          </div>
+        </div>
+      )}
     </div>
   ) : (
-    <div className="overflow-hidden whitespace-nowrap">
-      <div className="animate-scrollText inline-block flex-row [--scroll-duration:30s] [--scroll-start:45%]">
-        <span className="mr-100">
-          <TruckIcon className="mx-1 inline-block h-5 w-5" />
-          Lieferung ab 15€ Bestellwert frei Haus
-        </span>
-        <span className="mr-100">
-          Taglich: 12:00-22:00 Uhr Sonn- & Feiertage: 13:00-22:00 Uhr
-        </span>
-        <span className="mr-100">
-          <MapPinIcon className="mx-1 inline-block h-5 w-5" />
-          Bachstrase 2 • 58762 Altena
-        </span>
-        <span className="">
-          <PhoneIcon className="mx-1 inline-block h-5 w-5" />
-          02352 - 2670780
-        </span>
+    <div className="relative overflow-hidden">
+      <div
+        className={`${styles['scrolling-content-vertically']} flex flex-col space-y-4`}
+      >
+        <InfoAboutUs />
+        <InfoAboutUs /> {/* Duplicate for seamless looping */}
       </div>
     </div>
   );
 }
+
+const InfoAboutUs = () => (
+  <>
+    <table className="mx-auto table-auto border-separate border-spacing-x-5 border-spacing-y-1">
+      <caption className="caption-top">
+        <TruckIcon className="mx-1 inline-block h-5 w-5" />
+        Lieferung ab 15€ Bestellwert frei Haus
+      </caption>
+      <tbody>
+        <tr>
+          <td>Taglich:</td>
+          <td>12:00-22:00 Uhr</td>
+        </tr>
+        <tr>
+          <td>Sonn- & Feiertage:</td>
+          <td>13:00-22:00 Uhr</td>
+        </tr>
+      </tbody>
+      <caption className="caption-bottom">
+        <MapPinIcon className="mx-1 inline-block h-5 w-5" />
+        Bachstrase 2 • 58762 Altena
+      </caption>
+    </table>
+    <p>
+      <PhoneIcon className="mx-1 inline-block h-5 w-5" />
+      02352 - 2670780
+    </p>
+  </>
+);
+
+const CurrentlyClosedAlert = () => (
+  <p className="items-center whitespace-nowrap text-red-300 animate-pulse">
+    <AlertTriangle className="mr-8 inline-block h-5 w-5 animate-ping" />
+    Hinweis: Wir haben gerade geschlossen. Wenn Sie möchten, können Sie aber
+    jetzt schon bestellen, wir liefern dann zu unseren Geschäftszeiten.
+  </p>
+);
--- app/routes/app_layout.tsx
+++ app/routes/app_layout.tsx
@@ -1,13 +1,14 @@
-import { useState } from "react";
-import { Outlet } from "react-router";
-import { Toaster } from "sonner";
-import AboutUs from "~/components/about-us/AboutUs";
+import { useState } from 'react';
+import { Outlet } from 'react-router';
+import { Toaster } from 'sonner';
+import AboutUs from '~/components/about-us/AboutUs';
 // import FAB from "~/components/floating-action-button/FAB";
-import Navbar from "~/components/nav-bar/NavBar";
-import SidePanel from "~/components/side-panel/SidePanel";
+import Navbar from '~/components/nav-bar/NavBar';
+import SidePanel from '~/components/side-panel/SidePanel';
+import type { Route } from './+types/app_layout';
 // import { ScrollArea } from "~/components/ui/scroll-area";
 
-export default function AppLayout() {
+export default function AppLayout({ loaderData }: Route.ComponentProps) {
   const [isCollapsed, setIsCollapsed] = useState(true);
 
   const toggleFooter = () => setIsCollapsed(!isCollapsed);
@@ -32,10 +33,20 @@ export default function AppLayout() {
       <footer
         className="cursor-pointer overflow-hidden bg-gray-800 p-4 text-center transition-all duration-300 ease-in-out"
         onClick={toggleFooter}
-        style={{ height: isCollapsed ? "10em" : "110em" }}
+        style={{ height: isCollapsed ? '10em' : '110em' }}
       >
-        <AboutUs isOpen={!isCollapsed} />
+        <AboutUs
+          isExpanded={!isCollapsed}
+          isCurrentlyClosed={!loaderData.isCurrentlyOpen}
+        />
       </footer>
     </div>
   );
+}
+
+export async function loader() {
+  const currentHour = new Date().getHours();
+  return {
+    isCurrentlyOpen: currentHour >= 12 && currentHour < 23,
+  };
 }
