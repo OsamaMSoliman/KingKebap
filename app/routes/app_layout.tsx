c45ce194e5870ca4c943154b43966efb18d2d801
Osama Soliman <Osama.M.Soliman@uni-bonn.de>
2025-04-21 23:10:13 +0200

    future todos

--- app/routes/app_layout.tsx
+++ app/routes/app_layout.tsx
@@ -27,6 +27,14 @@ export default function AppLayout({ loaderData }: Route.ComponentProps) {
         <Outlet />
         {/* </ScrollArea> */}
         {/* <FAB /> */}
+        {/* TODO: make it an app (WPA) */}
+        {/* TODO: Impressum & Datenschutz-AGB */}
+        {/* TODO:
+          ZUSATZSTOFFE
+          1.)mit Farbstoff 2.) mit Konservierungsstoff 3.)mit Antioxidationsmittel 4.) mit Geschmacksverstärker 5.) geschwefelt 6.) geschwärzt 7.) gewachst 8.)mit Phosphat 9.) mit Süßungsmittel *enthält eine Phenylalininquelle 10.) mit Koffein 11.) mit Chinin
+          ALLERGENE
+          A.) Glutenhaltige Getreide* B.)Krebstiere* C.) Eier* D.) Fisch* E.)Erdnüsse F.)Sojabohnen* G.) Milch* H.)Schalenfrüchte* I.)Sellerie* J.) Senf* K.)Sesamsamen* L.)Schwefeldioxid und Sulfite M.)Lupinen* N.) Weichtiere *= und daraus gewonene Erzeugnisse
+        */}
         <SidePanel />
         <Toaster />
       </main>
